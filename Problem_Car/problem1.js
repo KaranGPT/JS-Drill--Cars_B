a37e4d16c498043dab755f62307dcb4c322f81a3
KARAN GUPTA <karan.20gcebcs103@galgotiacollege.edu>
2024-04-16 23:38:42 +0530

    Create problem1.js

---
+++ Problem_Car/problem1.js
@@ -0,0 +1,17 @@
+// ==== Problem #1 ====
+
+/* The dealer can't recall the information for a car with an id of 33 on his lot. 
+Help the dealer find out which car has an id of 33 by calling a function that will return the data for that car. 
+Then log the car's year, make, and model in the console log in the format of:
+"Car 33 is a *car year goes here* *car make goes here* *car model goes here*"
+*/
+
+
+import inventory from '../Cars_Inventory/inventory.js';
+const findCarById = (carId) => {
+    const car = inventory.filter(car => car.id === carId);
+    return car;
+    
+}
+
+export default findCarById;
